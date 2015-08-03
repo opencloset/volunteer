edec505e373ff9eabac98c83060506b845f31e85
Hyungsuk Hong <aanoaa@gmail.com>
2015-08-03 18:23:14 +0900

    Add new Controller named 'Work'

---
+++ lib/Volunteer/Controller/Work.pm
@@ -0,0 +1,19 @@
+package Volunteer::Controller::Work;
+use Mojo::Base 'Mojolicious::Controller';
+
+has schema => sub { shift->app->schema };
+
+=head1 METHODS
+
+=head2 add
+
+    GET /works/new
+
+=cut
+
+sub add {
+    my $self = shift;
+    $self->render('work/add');
+}
+
+1;
