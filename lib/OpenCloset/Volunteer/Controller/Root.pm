8eceddf4cc9c17ba0c4d4f2baca86fb874bc95f8
Keedi Kim <keedi.k@gmail.com>
2021-02-02 16:24:13 +0900

    Show closed msg when registration is unavailable

--- lib/OpenCloset/Volunteer/Controller/Root.pm
+++ lib/OpenCloset/Volunteer/Controller/Root.pm
@@ -1,6 +1,8 @@
 package OpenCloset::Volunteer::Controller::Root;
 use Mojo::Base 'Mojolicious::Controller';
 
+use Path::Tiny;
+
 =head1 METHODS
 
 =head2 home
@@ -12,6 +14,27 @@ use Mojo::Base 'Mojolicious::Controller';
 
 sub home {
     my $self = shift;
+}
+
+=head2 closed
+
+    under /works/new
+
+=cut
+
+sub closed {
+    my $self = shift;
+
+    if ( -f "closed" ) {
+        my $message = path("closed")->slurp_utf8;
+        $self->render(
+            template => "root/closed",
+            message  => $message,
+        );
+        return;
+    }
+
+    return 1;
 }
 
 1;
